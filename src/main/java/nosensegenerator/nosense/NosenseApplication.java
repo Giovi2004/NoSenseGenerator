72d1518b7cd8931aa7c3531dba9817d9d5af7349
Leonardo Erta <leoerta04@gmail.com>
2025-05-13 21:38:28 +0200

    NEW - stuttura base progetto in spring

---
+++ src/main/java/nosensegenerator/nosense/NosenseApplication.java
@@ -0,0 +1,13 @@
+package nosensegenerator.nosense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nosensegenerator/nosense/NosenseApplicationTests.java
@@ -0,0 +1,13 @@
+package nosensegenerator.nosense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
