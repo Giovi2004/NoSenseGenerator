cbc93b468c624b970ef8f49f841f0f0e69b0076f
Leonardo Erta <leoerta04@gmail.com>
2025-05-15 10:46:00 +0200

    NEW - classe NoSenseController

--- src/main/java/nosensegenerator/nosense/NoSenseController.java
+++ src/main/java/nosensegenerator/nosense/NoSenseController.java
@@ -1,5 +1,50 @@
 package nosensegenerator.nosense;
 
 public class NoSenseController {
-    
+    //da sistemate i metodi delle analisi in base a quello che serve
+    private Sentence inputSentence;
+    private Sentence templateSentence;
+    private Sentence generatedSentence;
+    private Generator generator;
+    private Noun nouns;
+    private Verb verbs;
+    private Adjective adjectives;
+    public NoSenseController(){
+        this.inputSentence=null;
+        this.templateSentence=null;
+        this.generatedSentence=null;
+        this.generator=new Generator();
+        this.nouns=new Noun();
+        this.verbs=new Verb();
+        this.adjectives=new Adjective();
+    }
+    public void AnalyzeInputSentence(String input){
+        this.inputSentence=new Sentence(input);
+        //inserire il risultato nella inputSentence
+        //string result = Analyzer.analyzeStructure(inputSentence);
+        //return result;
+    }
+    public String GenerateTemplateSentence(){
+        //this.templateSentence=generator.generateTemplateSentence();
+        return this.templateSentence.getText();
+    }
+
+    //da vedere se gestire il tempo della frase
+    /*public String FillTemplateSentence(String time){
+        //this.generatedSentence=generator.fillTemplateSentence(this.templateSentence,this.inputSentence,this.nouns,this.verbs,this.adjectives,time);
+        return this.generatedSentence.getText();
+    }*/
+    public String FillTemplateSentence(){
+        //this.generatedSentence=generator.fillTemplateSentence(this.templateSentence,this.inputSentence,this.nouns,this.verbs,this.adjectives);
+        return this.generatedSentence.getText();
+    }
+    public String AnalyzeGeneratedSentenceToxicity(){
+        //Analyzer.analyzeToxicity(this.generatedSentence);
+        return "";
+    }
+    public void SaveTerms(){
+        //nouns.save(inputSentence.getNouns());
+        //verbs.save(inputSentence.getVerbs());
+        //adjectives.save(inputSentence.getAdjectives());
+    }
 }
--- src/main/java/nosensegenerator/nosense/Sentence.java
+++ src/main/java/nosensegenerator/nosense/Sentence.java
@@ -2,6 +2,10 @@ package nosensegenerator.nosense;
 
 public class Sentence {
     private String text;
+    private String[] verbs;
+    private String[] nouns;
+    private String[] adjectives;
+    //aggiungere i gli attributi per il resto dei risultati del analisi
     
     public Sentence(String text) {
         this.text = text;
@@ -11,6 +15,24 @@ public class Sentence {
     }
     public void setText(String text) {
         this.text = text;
+    }
+    public String[] getVerbs() {
+        return verbs;
+    }
+    public void setVerbs(String[] verbs) {
+        this.verbs = verbs;
+    }
+    public String[] getNouns() {
+        return nouns;
+    }
+    public void setNouns(String[] nouns) {
+        this.nouns = nouns;
+    }
+    public String[] getAdjectives() {
+        return adjectives;
+    }
+    public void setAdjectives(String[] adjectives) {
+        this.adjectives = adjectives;
     }
     
 
