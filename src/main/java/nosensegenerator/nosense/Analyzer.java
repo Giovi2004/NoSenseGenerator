80891735dbb9eebe4aaabe2b11788a90bae8a5ba
Corbellino <nicola.tomasi.3@studenti.unipd.it>
2025-05-17 05:27:43 +0200

    NEW - Analyze Syntax API

--- src/main/java/nosensegenerator/nosense/Analyzer.java
+++ src/main/java/nosensegenerator/nosense/Analyzer.java
@@ -1,5 +1,44 @@
 package nosensegenerator.nosense;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.google.cloud.language.v1.AnalyzeSyntaxRequest;
+import com.google.cloud.language.v1.AnalyzeSyntaxResponse;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.EncodingType;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Token;
+
 public class Analyzer {
-    
+    private String text;
+
+    public static List<Token> analyzeSyntax(String sentence) throws IOException {
+
+        try (LanguageServiceClient language = LanguageServiceClient.create()) {
+            Document doc = Document.newBuilder()
+                    .setContent(sentence)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+
+            AnalyzeSyntaxRequest request = AnalyzeSyntaxRequest.newBuilder()
+                    .setDocument(doc)
+                    .setEncodingType(EncodingType.UTF16)
+                    .build();
+
+            AnalyzeSyntaxResponse response = language.analyzeSyntax(request);
+
+            // (Optional) print output
+            for (Token token : response.getTokensList()) {
+                System.out.printf("Text: %s, POS: %s, Lemma: %s\n",
+                        token.getText().getContent(),
+                        token.getPartOfSpeech().getTag(),
+                        token.getLemma());
+            }
+
+            return response.getTokensList(); // Return full token data
+        }
+    }
+
 }
--- src/main/java/nosensegenerator/nosense/NosenseApplication.java
+++ src/main/java/nosensegenerator/nosense/NosenseApplication.java
@@ -1,4 +1,9 @@
 package nosensegenerator.nosense;
+/* 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;*/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +12,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NosenseApplication {
 
 	public static void main(String[] args) {
+		
+		
+		/*URL resource = NosenseApplication.class.getClassLoader().getResource("keys/google-nlp-key.json");
+		if (resource == null) {
+			throw new RuntimeException("Could not find the Google NLP key in classpath!");
+		}
+		File keyFile = Paths.get(resource.toURI()).toFile();
+		System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", keyFile.getAbsolutePath());
+		String sentence = "The dog is barking at the mailman";
+		Analyzer analyzer = new Analyzer();
+		try {
+			analyzer.analyzeSyntax(sentence);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+
+
 		SpringApplication.run(NosenseApplication.class, args);
 	}
 
