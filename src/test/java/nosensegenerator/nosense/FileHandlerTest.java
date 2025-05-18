769601ae4e9f6f30b42dd2ebb9ddc2b3e5952254
Giovi2004 <giovanni.panziera@gmail.com>
2025-05-18 17:50:11 +0200

    NEW-fileHandlerTest e piccoli edit sugli altri test

---
+++ src/test/java/nosensegenerator/nosense/FileHandlerTest.java
@@ -0,0 +1,37 @@
+package nosensegenerator.nosense;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FileHandlerTest {
+
+    @BeforeAll
+    public static void setUp(){
+        try {
+            Files.write(Paths.get("src/test/java/nosensegenerator/nosense/test.txt"), "test\n".getBytes());
+        } catch (Exception e) {}
+    }
+         
+
+    @Test
+    public void saveloadTest() {
+        ArrayList<String> List = new ArrayList<>();
+        List.add("test");
+        ArrayList<String> sampleList = new ArrayList<>();
+        sampleList.add("test");
+        sampleList.add("test1");
+        sampleList.add("test2");
+        sampleList.add("test3");
+
+        FileHandler.save(List, sampleList, "src/test/java/nosensegenerator/nosense/test.txt");
+        assertThat(FileHandler.load("src/test/java/nosensegenerator/nosense/test.txt") , hasItems("test", "test1", "test2", "test3"));
+               
+    }
+}
--- src/test/java/nosensegenerator/nosense/NounTest.java
+++ src/test/java/nosensegenerator/nosense/NounTest.java
@@ -7,16 +7,12 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class NounTest {
-    @BeforeAll 
-    public static void setUp() {
-
-    }
+    
     @Test
     public void testGetNoun() {
         ArrayList<String> wordsFromFile = FileHandler.load("src/main/resources/terms/nouns.txt");
@@ -38,7 +34,6 @@ public class NounTest {
         if (lines.size() >= 2) {
             lines.remove(lines.size() - 1);
             Files.write(Paths.get("src/main/resources/terms/nouns.txt"), lines);
-            System.out.println("Penultimate line deleted successfully.");
         }
 
         } catch (Exception e) {}
